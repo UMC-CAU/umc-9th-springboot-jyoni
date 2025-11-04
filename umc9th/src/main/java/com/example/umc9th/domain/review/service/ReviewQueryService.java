74b5b759664d911bf3b099f51085c753f794e8e6
wldy4627 <wldy4627@gmail.com>
2025-11-05 02:37:55 +0900

    feat: ReviewQueryService에 내가 쓴 리뷰 목록 조회 로직 추가

---
+++ umc9th/src/main/java/com/example/umc9th/domain/review/service/ReviewQueryService.java
@@ -0,0 +1,36 @@
+package com.example.umc9th.domain.review.service;
+
+import com.example.umc9th.domain.review.dto.ReviewResponse;
+import com.example.umc9th.domain.review.entity.QReview;
+import com.example.umc9th.domain.review.repository.ReviewRepository;
+import com.querydsl.core.BooleanBuilder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReviewQueryService {
+
+    private final ReviewRepository reviewRepository;
+
+    public ReviewQueryService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    public List<ReviewResponse> getMyReviews(Long userId, String storeName, Integer ratingGroup) {
+        QReview review = QReview.review;
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (storeName != null) {
+            builder.and(review.store.name.eq(storeName));
+        }
+        if (ratingGroup != null) {
+            double lowerBound = ratingGroup;
+            double upperBound = Math.min(ratingGroup + 0.9, 5.0);
+
+            builder.and(review.rating.between(lowerBound, upperBound));
+        }
+
+        return reviewRepository.findMyReviewsByFilter(builder);
+    }
+}
